d5565503b13b0ec56c28fde40e1126f576a672cd
bangyoon <yoonb720@naver.com>
2020-05-14 01:21:21 +0900

    Add FindObject function to make TTS

---
+++ Be-My-Eyes/CoreMLHelpers/Probs.swift
@@ -0,0 +1,124 @@
+//
+//  AppDelegate.swift
+//  Be-My-Eyes
+//
+//  Created by Be-My-Eyes on 04/28/20.
+//  Copyright © 2020 Be-My-Eyes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreML
+
+/// Convert probability tensor into an image
+func codesToImage(_ _probs: MLMultiArray) -> UIImage? {
+    // TODO: dynamically load a label map instead of hard coding
+    // can this bonus data be included in the model file?
+    let label_map = [
+        0:  [255, 0, 0],
+        1:  [70, 70, 70],
+        2:  [0, 0, 142],
+        3:  [153, 153, 153],
+        4:  [190, 153, 153],
+        5:  [220, 20, 60],
+        6:  [128, 64, 128],
+        7:  [244, 35, 232],
+        8:  [220, 220, 0],
+        9:  [70, 130, 180],
+        10: [107, 142, 35],
+        11: [0, 0, 0]
+    ]
+    //5 : 빨강
+    //6 : 도로
+    //7 : 인도
+    
+    // convert the MLMultiArray to a MultiArray
+    let codes = MultiArray<Float32>(_probs)
+    // get the shape information from the probs
+    let height = codes.shape[1]
+    let width = codes.shape[2]
+    // initialize some bytes to store the image in
+    var bytes = [UInt8](repeating: 255, count: height * width * 4)
+    // iterate over the pixels in the output probs
+    
+    //print(label_map[Int(codes[0, 20, 20])]) //출력 형식 : Optional([128, 64, 128])
+    // print(Int(codes[0, 20, 20])) //출력 형식 : key값 int 숫자
+    for h in 0 ..< height {
+        for w in 0 ..< width {
+            // get the array offset for this word
+            let offset = h * width * 4 + w * 4
+            // get the RGB value for the highest probability class
+            let rgb = label_map[Int(codes[0, h, w])]
+            // set the bytes to the RGB value and alpha of 1.0 (255)
+            bytes[offset + 0] = UInt8(rgb![0])
+            bytes[offset + 1] = UInt8(rgb![1])
+            bytes[offset + 2] = UInt8(rgb![2])
+        }
+    }
+    // create a UIImage from the byte array
+    return UIImage.fromByteArray(bytes, width: width, height: height,
+                                 scale: 0, orientation: .up,
+                                 bytesPerRow: width * 4,
+                                 colorSpace: CGColorSpaceCreateDeviceRGB(),
+                                 alphaInfo: .premultipliedLast)
+}
+
+func FindObject(_ _probs: MLMultiArray) -> UIImage? {
+    // TODO: dynamically load a label map instead of hard coding
+    // can this bonus data be included in the model file?
+    let label_map = [
+        0:  [255, 0, 0],
+        1:  [70, 70, 70],
+        2:  [0, 0, 142],
+        3:  [153, 153, 153],
+        4:  [190, 153, 153],
+        5:  [220, 20, 60],
+        6:  [128, 64, 128],
+        7:  [244, 35, 232],
+        8:  [220, 220, 0],
+        9:  [70, 130, 180],
+        10: [107, 142, 35],
+        11: [0, 0, 0]
+    ]
+    //5 : 빨강
+    //6 : 도로
+    //7 : 인도
+    
+    // convert the MLMultiArray to a MultiArray
+    let codes = MultiArray<Float32>(_probs)
+    // get the shape information from the probs
+    let height = codes.shape[1]
+    let width = codes.shape[2]
+    
+    let key = Int(codes[0, height/2, width/2])
+    var text = ""
+    
+    if key == 5 {
+        text = "There is a person. Move to right"
+    } else if key == 1 {
+        text = "There is a car. Move to left"
+    }
+    
+    
+    // initialize some bytes to store the image in
+    var bytes = [UInt8](repeating: 255, count: height * width * 4)
+    // iterate over the pixels in the output probs
+    for h in 0 ..< height {
+        for w in 0 ..< width {
+            // get the array offset for this word
+            let offset = h * width * 4 + w * 4
+            // get the RGB value for the highest probability class
+            let rgb = label_map[Int(codes[0, h, w])]
+            // set the bytes to the RGB value and alpha of 1.0 (255)
+            bytes[offset + 0] = UInt8(rgb![0])
+            bytes[offset + 1] = UInt8(rgb![1])
+            bytes[offset + 2] = UInt8(rgb![2])
+        }
+    }
+    // create a UIImage from the byte array
+    return UIImage.fromByteArray(bytes, width: width, height: height,
+                                 scale: 0, orientation: .up,
+                                 bytesPerRow: width * 4,
+                                 colorSpace: CGColorSpaceCreateDeviceRGB(),
+                                 alphaInfo: .premultipliedLast)
+}
