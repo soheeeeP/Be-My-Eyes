47825d22554327c6523a9b14790b033d44fb4eef
bangyoon <yoonb720@naver.com>
2020-07-07 23:41:50 +0900

    Add RobotControlView.swift

---
+++ Be-My-Eyes/RobotControlView.swift
@@ -0,0 +1,29 @@
+//
+//  RobotControlView.swift
+//  Be-My-Eyes
+//
+//  Created by 방윤 on 2020/07/07.
+//  Copyright © 2020 Be-My-Eyes. All rights reserved.
+//
+
+import UIKit
+
+class RobotControlView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonDown(_ sender: Any) {
+    }
+    
+    @IBAction func buttonUp(_ sender: UIButton) {
+    }
+    
+    @IBAction func connectButtonPressed(_ sender: UIButton) {
+    }
+    
+
+}
+
