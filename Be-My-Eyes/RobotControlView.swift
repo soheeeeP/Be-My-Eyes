fe89416ce70570deba9ae6b576a5c2b3b477e834
bangyoon <yoonb720@naver.com>
2020-07-07 23:54:00 +0900

    Add Robot Control code

--- Be-My-Eyes/RobotControlView.swift
+++ Be-My-Eyes/RobotControlView.swift
@@ -7,21 +7,32 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class RobotControlView: UIViewController {
 
+    var stop = "stop"
+    var direction: [Int: String] = [0: "forward",
+                                    1: "backward",
+                                    2: "left",
+                                    3: "right"]
+    let mqttClient = CocoaMQTT(clientID: "BeMyEyes", host:"IPAddressHere", port:1883)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func buttonDown(_ sender: Any) {
+    @IBAction func buttonDown(_ sender: UIButton) {
+        print("Sending message: \(direction[sender.tag]!)")
     }
     
     @IBAction func buttonUp(_ sender: UIButton) {
+        print("Sending message: \(stop)")
     }
     
     @IBAction func connectButtonPressed(_ sender: UIButton) {
+        print("Sending message: \(direction[sender.tag]!)")
     }
     
 
