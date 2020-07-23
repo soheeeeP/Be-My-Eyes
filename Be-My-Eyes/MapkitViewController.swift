a1055b12c089cbfcae6d3a8d9543a75b02d95c70
hangil-lee <hangil0216@gmail.com>
2020-07-23 05:02:59 -0700

    ADD STT

--- Be-My-Eyes/MapkitViewController.swift
+++ Be-My-Eyes/MapkitViewController.swift
@@ -11,8 +11,9 @@ import Contacts
 import MapKit
 import CoreLocation
 import AVFoundation
+import Speech
 
-class MapkitViewController: UIViewController,  MKMapViewDelegate, CLLocationManagerDelegate {
+class MapkitViewController: UIViewController,  MKMapViewDelegate, CLLocationManagerDelegate, SFSpeechRecognizerDelegate {
 
     @IBOutlet weak var searchText: UITextField!
     var matchingItems: [MKMapItem] = [MKMapItem]()
@@ -43,6 +44,100 @@ class MapkitViewController: UIViewController,  MKMapViewDelegate, CLLocationMana
     //Finder
     var flag3 = false
     
+    // STT
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var myTextView: UITextField!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ko-KR"))
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    @IBAction func STT(_ sender: Any) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            button.isEnabled = false
+            button.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            button.setTitle("Stop Recording", for: .normal)
+        }
+    }
+    func startRecording() {
+         
+         if recognitionTask != nil {
+             recognitionTask?.cancel()
+             recognitionTask = nil
+         }
+         
+         let audioSession = AVAudioSession.sharedInstance()
+         do {
+            try audioSession.setCategory(AVAudioSession.Category.multiRoute)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+         } catch {
+             print("audioSession properties weren't set because of an error.")
+         }
+         
+         recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+         
+         let inputNode = audioEngine.inputNode
+        
+         guard let recognitionRequest = recognitionRequest else {
+             fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+         }
+         
+         recognitionRequest.shouldReportPartialResults = true
+         
+         recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+             
+             var isFinal = false
+             
+             if result != nil {
+                 
+                 self.myTextView.text = result?.bestTranscription.formattedString
+                 isFinal = (result?.isFinal)!
+             }
+             
+             if error != nil || isFinal {
+                 self.audioEngine.stop()
+                 inputNode.removeTap(onBus: 0)
+                 
+                 self.recognitionRequest = nil
+                 self.recognitionTask = nil
+                 
+                 self.button.isEnabled = true
+             }
+         })
+         
+         let recordingFormat = inputNode.outputFormat(forBus: 0)
+         inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+             self.recognitionRequest?.append(buffer)
+         }
+         
+         audioEngine.prepare()
+         
+         do {
+             try audioEngine.start()
+         } catch {
+             print("audioEngine couldn't start because of an error.")
+         }
+         
+         myTextView.text = "Say something, I'm listening!"
+         
+     }
+     
+     
+    
+     func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+         if available {
+             button.isEnabled = true
+         } else {
+             button.isEnabled = false
+         }
+     }
     // Go Back to main menu
     @IBAction func GoBack(_ sender: Any) {
         distanceTimer!.invalidate()
@@ -74,6 +169,8 @@ class MapkitViewController: UIViewController,  MKMapViewDelegate, CLLocationMana
         
         distanceTimer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(MapkitViewController.distanceCheck), userInfo: nil, repeats: true)
         directionTimer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(MapkitViewController.getDirections), userInfo: nil, repeats: true)
+        
+        speechRecognizer?.delegate = self
     }
     
     // mapView
