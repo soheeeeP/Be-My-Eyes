4b7c8ad073d42d38ff99a0e222aa2355fd2d9aaf
bangyoon <yoonb720@naver.com>
2020-08-05 14:08:57 +0900

    Depth setting for iPhoneX

--- Be-My-Eyes/ViewController.swift
+++ Be-My-Eyes/ViewController.swift
@@ -293,7 +293,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, AVCaptureVide
         
         // get a handle on the back depth camera
         /// iPhone11 : builtInDualWideCamera   iPhoneX : builtInDualCamera   iPhone8 : builtInWideAngleCamera
-        guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+        guard let camera = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) else {
             let message = "No depth video camera available"
             popup_alert(self, title: "Camera error", message: message)
             return
